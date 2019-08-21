6c9eb6a4f7212ef9c9450bfda0361970b4aa829b
sabelogumede <iamsabelogumede@gmail.com>
2019-08-21 21:16:13 +0200

    component fix

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,76 @@
+// lifecycle hook OnInit import
+import { Component, OnInit } from '@angular/core';
+// my product interface - strong typing object
+import { IProduct } from './product';
+
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+// implement Onint lifecyle interface
+export class ProductListComponent implements OnInit {
+    pageTitle = 'Product List';
+    imageWidth = 50;
+    imageMargin = 2;
+    showImage = false;
+
+    // filter getter/setter
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    // filtered product list
+    filteredProducts: IProduct[];
+    // product data
+    products: IProduct[] = [
+        {
+            "productId": 1,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2019",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.2,
+            "imageUrl": "assets/images/leaf_rake.png"
+          },
+          {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2019",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+          }
+    ];
+
+    // constructor taking inn our filter
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+    // perform-form-filter function called in setter conditional logic
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+    // interface hook
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
+
+
+}
