1a07eb17c3df536d478b49da802f259d18e1c993
sabelogumede <iamsabelogumede@gmail.com>
2019-08-22 08:56:33 +0200

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-=======
 import { BrowserModule } from '@angular/platform-browser';
->>>>>>> ceffa79f80fc7f063f13973104625fa6ffbacd16
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
