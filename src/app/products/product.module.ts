8a1f1f4836220880145f4f91244fa5f6f4c33a75
sabelogumede <iamsabelogumede@gmail.com>
2019-09-09 14:03:23 +0200

    aaded shared module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,25 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ConvertToSpacesPipe
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       {
@@ -27,7 +23,8 @@ import { ProductDetailGuard } from './product-detail.guard';
         canActivate: [ProductDetailGuard],
         component: ProductDetailComponent
       },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
