f491f62dcc49200730b786bba962c48ac257c49d
sabelogumede <iamsabelogumede@gmail.com>
2019-09-09 11:42:44 +0200

    added product detail guard

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -11,9 +12,27 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-  }
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    // use http instead hardcoded data
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-001',
+      'releaseDate': 'March 19, 2019',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png'
+      }
+    }
+    // navigate back method
+    onBack(): void {
+      this.router.navigate(['/products']);
+    }
 
-}
+  }
