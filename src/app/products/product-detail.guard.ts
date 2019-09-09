fba32c5f41d79cc40c07d0ca774fc80637dbb869
sabelogumede <iamsabelogumede@gmail.com>
2019-09-09 13:48:26 +0200

    added a feature module as part of refacturing

--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -12,7 +12,7 @@ export class ProductDetailGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      let id = +next.url[1].path;
+      const id = +next.url[1].path;
       if (isNaN(id) || id < 1) {
         alert('Invalid product Id');
         this.router.navigate(['/products']);
